4855bb9e369009ea80b976bec5204046dae22ce8
Alvf <alvin.shi@gmail.com>
2021-12-07 18:33:52 -0500

    making server create a log.txt of keystrokes.

--- server.c
+++ server.c
@@ -12,7 +12,9 @@
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
-   
+
+FILE *fd;
+
 // Function designed for chat between client and server.
 void read_keystrokes(int sockfd)
 {
@@ -21,11 +23,12 @@ void read_keystrokes(int sockfd)
     // infinite loop for chat
     for (;;) {
         bzero(buff, MAX);
-   
+
         // read the message from client and copy it in buffer
         read(sockfd, buff, sizeof(buff));
         // print buffer which contains the client contents
         printf("From client: %s\n", buff);
+        fputs(buff,fd);  
         bzero(buff, MAX);
 	n = 0;
         // copy server message in the buffer
@@ -33,13 +36,13 @@ void read_keystrokes(int sockfd)
             ;
     }
 }
-   
+
 // Driver function
 int main()
 {
     int sockfd, connfd, len;
     struct sockaddr_in servaddr, cli;
-   
+
     // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
@@ -49,12 +52,12 @@ int main()
     else
         printf("Socket successfully created..\n");
     bzero(&servaddr, sizeof(servaddr));
-   
+
     // assign IP, PORT
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
-   
+
     // Binding newly created socket to given IP and verification
     if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
         printf("socket bind failed...\n");
@@ -62,7 +65,7 @@ int main()
     }
     else
         printf("Socket successfully binded..\n");
-   
+
     // Now server is ready to listen and verification
     if ((listen(sockfd, 5)) != 0) {
         printf("Listen failed...\n");
@@ -71,7 +74,7 @@ int main()
     else
         printf("Server listening..\n");
     len = sizeof(cli);
-   
+
     // Accept the data packet from client and verification
     connfd = accept(sockfd, (SA*)&cli, &len);
     if (connfd < 0) {
@@ -80,10 +83,15 @@ int main()
     }
     else
         printf("server accept the client...\n");
-   
+
+    fd = fopen("log.txt","a");
+    if(!fd){
+      printf("creating log.txt failed.\n");
+    }
+
     // Function for chatting between client and server
     read_keystrokes(connfd);
-   
+
     // After chatting close the socket
     close(sockfd);
 }
